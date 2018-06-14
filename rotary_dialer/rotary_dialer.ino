a96449928a1d3e342eb15737ed992eee8bef7206
Ákos Muráti <akos@murati.hu>
2018-06-14 23:43:16 +1200

    Add serial debug mode

--- rotary_dialer/rotary_dialer.ino
+++ rotary_dialer/rotary_dialer.ino
@@ -1,27 +1,41 @@
 /* Skeleton for HID */
 #include "USBKeyboard.h"
 
+// Wired pins - White - Ground
+const int debugPin =  0; //For serial debugging mode
+const int dialPin =  0; // Green-rotary 
+const int counterPin =  0; //Brown-rotary
+
 const int ledPin =  13;
 
-
-void setup() 
-{
-  pinMode(ledPin, OUTPUT);
+void setup() {
+  //pinMode(ledPin, OUTPUT);
+  pinMode(debugPin, INPUT);
   Keyboard.init();
 }
 
-void loop()
-{
-  uint8_t ledStatus;
-  ledStatus = Keyboard.readLedStatus();
+void loop() {
+  delay(500);
+  
+  // Read controls
+  int isDebug = 1 - digitalRead(debugPin);
+  int isDialing = digitalRead(dialPin);
+  int isCounting = digitalRead(counterPin);
+  
+  if (isDebug) {
+    Serial.println("DEBUG=1");
+  }
+  
+  //uint8_t ledStatus;
+  //ledStatus = Keyboard.readLedStatus();
 
-  if (ledStatus & LED_CAPSLOCK) {
-    digitalWrite(ledPin, HIGH);      
-  } else {
-    digitalWrite(ledPin, LOW);
-  } 
+  // if (ledStatus & LED_CAPSLOCK) {
+  //   digitalWrite(ledPin, HIGH);      
+  // } else {
+  //   digitalWrite(ledPin, LOW);
+  // } 
 
-  delay(3000);
-  Keyboard.print("Akos");
-  Keyboard.sendKeyStroke(KEY_ENTER);     // Send keystroke
+  
+  //Keyboard.print("Akos");
+  //Keyboard.sendKeyStroke(KEY_ENTER);     // Send keystroke
 }
