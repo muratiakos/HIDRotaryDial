59fa9ffb3566cbf69f14cc3cb6d45892a8abc2f4
Ákos Muráti <akos@murati.hu>
2018-06-15 00:55:30 +1200

    Add dial, counter

--- rotary_dialer/rotary_dialer.ino
+++ rotary_dialer/rotary_dialer.ino
@@ -2,28 +2,42 @@
 #include "USBKeyboard.h"
 
 // Wired pins - White - Ground
-const int debugPin =  0; //For serial debugging mode
-const int dialPin =  0; // Green-rotary 
-const int counterPin =  0; //Brown-rotary
+const int debugPin =  2; //For serial debugging mode
+const int dialerPin =  3; // Green-rotary 
+const int counterPin =  4; //Brown-rotary
 
 const int ledPin =  13;
 
+byte counter;
+
 void setup() {
-  //pinMode(ledPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
   pinMode(debugPin, INPUT);
+  pinMode(dialerPin, INPUT);
+  pinMode(counterPin, INPUT);
+
   Keyboard.init();
+  counter = 0;
 }
 
 void loop() {
-  delay(500);
-  
+  delay(10);
+
   // Read controls
-  int isDebug = 1 - digitalRead(debugPin);
-  int isDialing = digitalRead(dialPin);
+  int isDebug = digitalRead(debugPin);
+  int isDialing = digitalRead(dialerPin);
   int isCounting = digitalRead(counterPin);
   
-  if (isDebug) {
-    Serial.println("DEBUG=1");
+  if (1 || isDebug) {
+    counter++;
+    Serial.print(counter);
+    Serial.print("\t");
+    Serial.print(isDebug);
+    Serial.print("\t");
+    Serial.print(isDialing);
+    Serial.print("\t");
+    Serial.print(isCounting);
+    Serial.print("\n");
   }
   
   //uint8_t ledStatus;
