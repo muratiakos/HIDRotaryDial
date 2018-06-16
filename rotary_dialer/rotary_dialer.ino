12fcd200b59d497a9b39183b9bbe8a234222c43f
Ákos Muráti <akos@murati.hu>
2018-06-16 23:59:39 +1200

    Add counter

--- rotary_dialer/rotary_dialer.ino
+++ rotary_dialer/rotary_dialer.ino
@@ -1,14 +1,17 @@
 /* Skeleton for HID */
 #include "USBKeyboard.h"
 
-// Wired pins - White - Ground
+// Wired pins
+// GND - ROTARY - White
 const int debugPin =  2; //For serial debugging mode
-const int dialerPin =  3; // Green-rotary 
-const int counterPin =  4; //Brown-rotary
+const int dialerPin =  3; // ROTARY - GREEN
+const int counterPin =  4; // ROTARY - BROWN
 
 const int ledPin =  13;
 
-byte counter;
+byte window;
+byte counter = 0;
+byte last_counter_bit = 0;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
@@ -17,28 +20,43 @@ void setup() {
   pinMode(counterPin, INPUT_PULLUP);
 
   Keyboard.init();
-  counter = 0;
+  window = 0;
 }
 
 void loop() {
   delay(10);
-  counter++;
+  window++;
 
   // Read controls
   int isDebug = 1 - digitalRead(debugPin);
   int isDialing = 1 - digitalRead(dialerPin);
   int isCounting = digitalRead(counterPin);
+
+  // Count the dials for the edges
+  if (!isDialing) { // Reset counter
+    counter = 0;
+  }
+  else if (isDialing && isCounting && (1-last_counter_bit)) {
+    counter++;
+  } 
   
-  if (isDebug && isDialing) {
-    
-    Serial.print(counter);
-    Serial.print("\t");
-    Serial.print(isDebug);
-    Serial.print("\t");
-    Serial.print(isDialing);
-    Serial.print("\t");
-    Serial.print(isCounting);
-    Serial.print("\n");
+  //Get debug info
+  if (isDebug) {
+    // Only show 
+    if (isDialing) {
+      Serial.print(window);
+      Serial.print("\t");
+      Serial.print(isDebug);
+      Serial.print("\t");
+      Serial.print(isDialing);
+      Serial.print("\t");
+      Serial.print(isCounting);
+      Serial.print("\t");
+      Serial.print(last_counter_bit);
+      Serial.print("\t");
+      Serial.print(counter);
+      Serial.print("\n");
+    }
   }
   
   //uint8_t ledStatus;
@@ -53,4 +71,7 @@ void loop() {
   
   //Keyboard.print("Akos");
   //Keyboard.sendKeyStroke(KEY_ENTER);     // Send keystroke
+
+  // Capture the last state
+  last_counter_bit = isCounting;
 }
